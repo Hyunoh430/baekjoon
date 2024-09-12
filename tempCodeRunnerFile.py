6b49ce4391fe5a93b24ea147492eee77b6410d5a
현오홍 <harrymickey@naver.com>
2024-09-12 19:18:23 +0900

    dfs bfs

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+for i in range(0, len(n)/2):
+#     sum1 += int(n[i])
+# for i in range(len(n)/2, len(n)):
+#     sum2 += int(n[i])
+# print(sum1)
---
+++ 프로그래머스 타겟넘버 bfs.py
@@ -0,0 +1,10 @@
+sizes = [
+    [60, 50],
+    [30, 70],
+    [60, 30],
+    [80, 40]
+]
+
+print(list(row[0] for row in sizes))
+
+
