60c21110490846574ccbdd6188b0905fc457699e
hyunoh <harrymickey@naver.com>
2025-09-23 10:00:12 +0900

    Changes

--- 백준 9205.py
+++ 백준 9205.py
@@ -11,27 +11,31 @@ for _ in range(t):
         c_x, c_y = map(int, input().split())
         conv.append([c_x, c_y])
     f_x, f_y = map(int, input().split())
-
+    #print(s_x, s_y, conv, f_x, f_y)
     #1 시작지점과 편의점지점이 거리 안에 있드면 끝!
     if abs(s_x - f_x) + abs(s_y - f_y) <= 1000:
         print("happy")
-        break
+        continue
 
     #2 거리 안에 없다면? 편의점 있나 확인
     q = deque()
     q.append([s_x, s_y])
     check = 0
+    visited = [0 for _ in range(n + 1)]
     while q:
+        #print(q)
         x, y = q.popleft()
         if abs(x - f_x) + abs(y - f_y) <= 1000:
             check = 1
             print("happy")
             break
         for i in range(n):
-            new_x, new_y = conv[i]
-            if abs(x - new_x) + abs(y - new_y) <= 1000:
-                q.append([new_x, new_y])
-    if check == 1:
+            if not visited[i]:
+                new_x, new_y = conv[i]
+                if abs(x - new_x) + abs(y - new_y) <= 1000:
+                    q.append([new_x, new_y])
+                    visited[i] = 1
+    if check == 0:
         print("sad")
 
 
